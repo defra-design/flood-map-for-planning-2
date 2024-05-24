00bbd8744776e7b0ffd7d6e984f5621d9875d65a
Joe Lanman <joelanman@gmail.com>
2024-05-24 12:36:36 +0100

    location search and map

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,10 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.use((req, res, next) => {
+    res.locals.osApiKey = process.env.OS_API_KEY
+    res.locals.styleUrl = process.env.STYLE_URL
+    res.locals.aerialUrl = process.env.AERIAL_URL
+    next()
+})
