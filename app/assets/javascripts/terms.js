e47f1fffca9b75f6a2c32729d9fd59123fc2f2de
Christina Gyles <christina.gyles@defra.gov.uk>
2025-01-11 07:41:43 +0000

    update to likelihood chance details and option for two seperate rows around percentage and chance event

--- app/assets/javascripts/terms.js
+++ app/assets/javascripts/terms.js
@@ -6,6 +6,22 @@ const terms = {
     swHigh: '3.3%',
     swMedium: '1%',
     swLow: '0.1%'
+  },
+  chance: {
+    rsHigh: '1 in 30',
+    rsMedium: 'Rivers 1 in 100, Sea 1 in 50',
+    rsLow: 'Rivers and sea 1 in 1000',
+    swHigh: '1 in 30',
+    swMedium: '1 in 100',
+    swLow: '1 in 1000'
+  },
+  likelihoodchance: {
+    rsHigh: 'Rivers and sea 3.3% (1 in 30)',
+    rsMedium: 'Rivers 1% Sea 0.5% (Rivers 1 in 100, Sea 1 in 50)',
+    rsLow: 'Rivers and sea 0.1% (1 in 1000)',
+    swHigh: '3.3% (1 in 30)',
+    swMedium: '1% (1 in 100)',
+    swLow: '0.1% (1 in 1000)'
   }
 }
 
