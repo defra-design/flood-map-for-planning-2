4b6185ab4acbce707ee522fed5cbfc9d1b94bda0
Christina Gyles <christina.gyles@defra.gov.uk>
2025-06-06 13:01:01 +0100

    updated in line with Data issue wider content

--- app/assets/javascripts/fmp-main.js
+++ app/assets/javascripts/fmp-main.js
@@ -504,7 +504,7 @@ getDefraMapConfig().then((defraMapConfig) => {
           },
           {
             id: 'fzcl',
-            label: 'Flood zones with climate change'
+            label: 'Flood zones 2 and 3 (2070 to 2125)'
           },
           // {
           //   id: 'rsd',
@@ -540,7 +540,7 @@ getDefraMapConfig().then((defraMapConfig) => {
           },
           {
             id: 'fzcl',
-            label: 'Flood zones with climate change'
+            label: '2070 to 2125'
           }
         ]
       },
@@ -973,7 +973,7 @@ getDefraMapConfig().then((defraMapConfig) => {
       if (floodZone === 'No data available') {
         contentFloodZones += '<p class="govuk-body-s"><strong>No data available</strong></p><p class="govuk-body-s">Climate change data is currently unavailable at this location. We will publish the data when it becomes available.</p>'
       } else if (mapState.isClimateChange) {
-        contentFloodZones += '<p class="govuk-body-s"><strong>How to use flood zones with climate change</strong></p> <p class="govuk-body-s">Flood zones plus climate change are given to help you further investigate flood risk. </br> <a href="data">Find out more about this data and how it should be used</a></p>'
+        contentFloodZones += '<p class="govuk-body-s"><strong>How to use flood zones with climate change</strong></p> <p class="govuk-body-s">Flood zones with climate change are given to help you further investigate flood risk. </br> <a href="data">Find out more about this data and how it should be used</a></p>'
       } else {
         contentFloodZones += '<p class="govuk-body-s"><strong>Updates to flood zones 2 and 3</strong></p> <p class="govuk-body-s">Flood zones 2 and 3 have been updated to include local detailed models, and a new improved national model.</p> '
        }
