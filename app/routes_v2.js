52f6e9cb922c4ed892c3f7752929652a98de91e3
Christina Gyles <christina.gyles@defra.gov.uk>
2025-09-03 12:59:43 +0100

    updates

--- app/routes_v2.js
+++ app/routes_v2.js
@@ -1,6 +1,5 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
- 
 
 var folder = "v2"
 var servicename = "Flood map for planning";
@@ -9,72 +8,71 @@ var servicename = "Flood map for planning";
 var backlink = '<a href="javascript:history.back()" class="govuk-back-link">Back</a>'
 
 // Add your routes here
-
 router.use(function (req, res, next) {
-  // set a folder and store in locals
-  // this can then be used in pages as {{folder}}
-  res.locals.folder=folder
-  res.locals.backlink=backlink
-  // permit and autostore data set in all statement at bottom
-  res.locals.permit=res.locals.data
+  res.locals.folder = folder
+  res.locals.backlink = backlink
+  res.locals.permit = res.locals.data
   next()
-});
+})
 
-// radio options to direct to map or triaging away from FMFP ============
+// =====================================
+// Location page
+// =====================================
+router.get('/location', function (req, res) {
+  res.render(folder + '/location', {
+    formAction: "/" + folder + "/location-check"
+  })
+})
+
+router.post('/location-check', function (req, res) {
+  const type = req.body.locationType
+
+  if (type === 'placePost') {
+    const query = req.body.locationPlacePost
+    res.redirect('/' + folder + '/map')
+
+  } else if (type === 'ngr') {
+    const query = req.body.locationNGR
+    res.redirect('/' + folder + '/map')
+
+  } else if (type === 'eastNorth') {
+    const e = req.body.easting
+    const n = req.body.northing
+    res.redirect('/' + folder + '/map')
+
+  } else {
+    // fallback if no selection
+    res.redirect('/' + folder + '/map')
+  }
+})
+
+// =====================================
+// Existing options-radio flow
+// =====================================
 router.get('/options-radio', function (req, res) {
-  res.render(folder + '/options-radio',{
-    "formAction":"/"+ folder + "/options-radio-check"
+  res.render(folder + '/options-radio', {
+    formAction: "/" + folder + "/options-radio-check"
   })
 })
 
-router.post('/location/catigories', function (req, res) {
-  res.render(folder + '/options-radio',{
-    "formAction":"/"+ folder + "/options-radio-check"
+router.post('/options-radio', function (req, res) {
+  res.render(folder + '/options-radio', {
+    formAction: "/" + folder + "/options-radio-check"
   })
 })
 
-// dirceting to regions
 router.post('/options-radio-check', function (req, res) {
-  if (req.body['triage']=="planning") {
+  if (req.body['triage'] == "planning") {
     res.redirect("/" + folder + "/location")
-} else if (req.body['triage']=="buySell"){
+  } else if (req.body['triage'] == "buySell") {
     res.redirect("https://www.gov.uk/check-long-term-flood-risk")
-} else if (req.body['triage']=="pastFlood"){
-  res.redirect("https://www.gov.uk/request-flooding-history")
-} else if (req.body['triage']=="insurance"){
-res.redirect("https://www.gov.uk/check-long-term-flood-risk")
-} else if (req.body['triage']=="notListed"){
-  res.redirect("https://www.gov.uk/browse/environment-countryside/flooding-extreme-weather")
-}
+  } else if (req.body['triage'] == "pastFlood") {
+    res.redirect("https://www.gov.uk/request-flooding-history")
+  } else if (req.body['triage'] == "insurance") {
+    res.redirect("https://www.gov.uk/check-long-term-flood-risk")
+  } else if (req.body['triage'] == "notListed") {
+    res.redirect("https://www.gov.uk/browse/environment-countryside/flooding-extreme-weather")
+  }
 })
 
-// // IN PROGRESS ============
-// router.get('/contact', function (req, res) {
-//   res.render(folder + '/contact',{
-//     "formAction":"/"+ folder + "/contact-check"
-//   })
-// })
-
-// router.post('/contact', function (req, res) {
-//   res.render(folder + '/contact',{
-//     "formAction":"/"+ folder + "/contact-check"
-//   })
-// })
-
-// // dirceting to correct flood zone
-// router.post('/contact-check', function (req, res) {
-//   var floodZone = req.body.floodZone
-
-//     if (req.session.data.floodZone==="fz1") {
-//       res.redirect("/"+ folder + "/summary-fz3")
-//     } else if (req.session.data.floodZone==="fz2"){
-//       res.redirect("/"+ folder + "/summary-fz3")
-//     } else if (req.session.data.floodZone==="fz3"){
-//       res.redirect("/"+ folder + "/summary-fz3")    
-//     } else if (req.session.data.floodZone==="fzd"){
-//       res.redirect("/"+ folder + "/summary-fzd")  
-//     }
-// })
-
-
-module.exports = router;
+module.exports = router
