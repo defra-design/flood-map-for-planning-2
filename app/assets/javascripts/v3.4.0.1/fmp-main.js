728bcf10d35c24e31024639e62c63774da24ac35
Mark Fee <mark.fee@defra.gov.uk>
2025-09-19 13:32:52 +0100

    removed some legacy MC config items, added in a helpLink and collapse

--- app/assets/javascripts/v3.4.0.1/fmp-main.js
+++ app/assets/javascripts/v3.4.0.1/fmp-main.js
@@ -278,6 +278,7 @@ getDefraMapConfig().then((defraMapConfig) => {
     height: '100%',
     hasGeoLocation: false,
     framework: 'esri',
+    helpURL: 'https://google.com',
     symbols: [symbols.waterStorageAreas, symbols.floodDefences, symbols.mainRivers, symbols.noData],
     transformSearchRequest: getRequest,
     interceptorsCallback: getInterceptors,
@@ -469,15 +470,11 @@ getDefraMapConfig().then((defraMapConfig) => {
       ]
     },
     queryArea: {
+      collapse: 'collapse',
       heading: 'Get a boundary report',
-      startLabel: 'Add site boundary',
-      editLabel: 'Edit site boundary',
-      addLabel: 'Add boundary',
-      updateLabel: 'Update boundary',
       submitLabel: 'Get summary report',
-      helpLabel: 'How to draw a shape',
       keyLabel: 'Report area',
-      html: '<h3 class="govuk-heading-m govuk-!-font-size-16">For an approximate site boundary</h3> <ul class="govuk-list govuk-list--bullet govuk-!-font-size-16"><li>use the red square to define the boundary of your site</li><li>zoom and move the map to position the square</li><li>click the ‘add boundary’ button to finish</li></ul></p></br><h3 class="govuk-heading-m govuk-!-font-size-16">For a more detailed site boundary:</h3><ul class="govuk-list govuk-list--bullet govuk-!-font-size-16"><li>click ‘edit shape’ and dots will appear on the square</li><li>move the dots to change the shape of the square until it matches your boundary</li><li>click the ‘add boundary’ button to finish</li></ul>',
+      summary: 'Add or edit site boundary',
       // minZoom: 21,
       // min zoom update to edit boundary zoom restriction
       maxZoom: 22,
@@ -487,6 +484,7 @@ getDefraMapConfig().then((defraMapConfig) => {
       onShapeUpdate: ({ area, geometry }) => {
         const isValid = area <= 1000000
         console.log({
+          geometry,
           warningText: !isValid ? 'Area too big' : null,
           allowShape: false
         })
