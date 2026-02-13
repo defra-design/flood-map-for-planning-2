f2d42de2c12f7b7cda98bbd4411533c30c045ee9
Mark Fee <mark.fee@defra.gov.uk>
2026-02-13 13:54:03 +0000

    Updated MC to v0.6.7

--- .tmp/public/javascripts/core-workers-request.js
+++ .tmp/public/javascripts/core-workers-request.js
@@ -1,25 +1,2 @@
-"use strict";
-(self["webpackChunkdefra"] = self["webpackChunkdefra"] || []).push([["core-workers-request"],{
-
-/***/ "./node_modules/@arcgis/core/core/workers/request.js":
-/*!***********************************************************!*\
-  !*** ./node_modules/@arcgis/core/core/workers/request.js ***!
-  \***********************************************************/
-/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   execute: function() { return /* binding */ e; }
-/* harmony export */ });
-/* harmony import */ var _Error_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Error.js */ "./node_modules/@arcgis/core/core/Error.js");
-/*
-All material copyright ESRI, All Rights Reserved, unless otherwise specified.
-See https://js.arcgis.com/4.31/esri/copyright.txt for details.
-*/
-function e(e,s){let a=s.responseType;a?"array-buffer"!==a&&"blob"!==a&&"json"!==a&&"native"!==a&&"native-request-init"!==a&&"text"!==a&&(a="text"):a="json",s.responseType=a;const r=s.signal;return delete s.signal,globalThis.invokeStaticMessage("request",{url:e,options:s},{signal:r}).then((async n=>{let o,i,l,u,c;if(n.data)if(n.data instanceof ArrayBuffer){if(!("json"!==a&&"text"!==a&&"blob"!==a||(o=new Blob([n.data]),"json"!==a&&"text"!==a||(u=await o.text(),"json"!==a)))){try{i=JSON.parse(u||null)}catch(p){const a={...p,url:e,requestOptions:s};throw new _Error_js__WEBPACK_IMPORTED_MODULE_0__["default"]("request:server",p.message,a)}if(i.error){const a={...i.error,url:e,requestOptions:s};throw new _Error_js__WEBPACK_IMPORTED_MODULE_0__["default"]("request:server",i.error.message,a)}}}else"native"===a&&(n.data.signal=r,l=await fetch(n.data.url,n.data),n.httpStatus=l.status);switch(a){case"blob":c=o;break;case"json":c=i;break;case"native":c=l;break;case"text":c=u;break;default:c=n.data}return{data:c,httpStatus:n.httpStatus,requestOptions:s,ssl:n.ssl,url:e}}))}
-
-
-/***/ })
-
-}]);
+"use strict";(self.webpackChunkdefra=self.webpackChunkdefra||[]).push([[4456],{80592:function(e,t,s){s.r(t),s.d(t,{execute:function(){return r}});var a=s(39819);function r(e,t){let s=t.responseType;s?"array-buffer"!==s&&"blob"!==s&&"json"!==s&&"native"!==s&&"native-request-init"!==s&&"text"!==s&&(s="text"):s="json",t.responseType=s;const r=t.signal;return delete t.signal,globalThis.invokeStaticMessage("request",{url:e,options:t},{signal:r}).then((async n=>{let o,u,i,l,c;if(n.data)if(n.data instanceof ArrayBuffer){if(!("json"!==s&&"text"!==s&&"blob"!==s||(o=new Blob([n.data]),"json"!==s&&"text"!==s||(l=await o.text(),"json"!==s)))){try{u=JSON.parse(l||null)}catch(s){const r={...s,url:e,requestOptions:t};throw new a.A("request:server",s.message,r)}if(u.error){const s={...u.error,url:e,requestOptions:t};throw new a.A("request:server",u.error.message,s)}}}else"native"===s&&(n.data.signal=r,i=await fetch(n.data.url,n.data),n.httpStatus=i.status);switch(s){case"blob":c=o;break;case"json":c=u;break;case"native":c=i;break;case"text":c=l;break;default:c=n.data}return{data:c,httpStatus:n.httpStatus,requestOptions:t,ssl:n.ssl,url:e}}))}}}]);
 //# sourceMappingURL=core-workers-request.js.map
--- .tmp/public/javascripts/previewWebStyleSymbol.js
+++ .tmp/public/javascripts/previewWebStyleSymbol.js
@@ -1,28 +1,2 @@
-"use strict";
-(self["webpackChunkdefra"] = self["webpackChunkdefra"] || []).push([["previewWebStyleSymbol"],{
-
-/***/ "./node_modules/@arcgis/core/symbols/support/previewWebStyleSymbol.js":
-/*!****************************************************************************!*\
-  !*** ./node_modules/@arcgis/core/symbols/support/previewWebStyleSymbol.js ***!
-  \****************************************************************************/
-/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   previewWebStyleSymbol: function() { return /* binding */ r; }
-/* harmony export */ });
-/* harmony import */ var _request_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../request.js */ "./node_modules/@arcgis/core/request.js");
-/* harmony import */ var _core_screenUtils_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../core/screenUtils.js */ "./node_modules/@arcgis/core/core/screenUtils.js");
-/* harmony import */ var _previewUtils_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./previewUtils.js */ "./node_modules/@arcgis/core/symbols/support/previewUtils.js");
-/* harmony import */ var _utils_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./utils.js */ "./node_modules/@arcgis/core/symbols/support/utils.js");
-/*
-All material copyright ESRI, All Rights Reserved, unless otherwise specified.
-See https://js.arcgis.com/4.31/esri/copyright.txt for details.
-*/
-function r(e,i,r){const h=e.thumbnail?.url;return h?(0,_request_js__WEBPACK_IMPORTED_MODULE_0__["default"])(h,{responseType:"image"}).then((t=>{const e=s(t.data,r);return r?.node?(r.node.appendChild(e),r.node):e})):(0,_utils_js__WEBPACK_IMPORTED_MODULE_3__.fetchWebStyleSymbol)(e).then((t=>t?i(t,r):null))}function s(t,n){const r=!/\\.svg$/i.test(t.src)&&n?.disableUpsampling,s=Math.max(t.width,t.height);let h=null!=n?.maxSize?(0,_core_screenUtils_js__WEBPACK_IMPORTED_MODULE_1__.pt2px)(n.maxSize):_previewUtils_js__WEBPACK_IMPORTED_MODULE_2__.SymbolSizeDefaults.maxSize;r&&(h=Math.min(s,h));const o="number"==typeof n?.size?n?.size:null,m=Math.min(h,null!=o?(0,_core_screenUtils_js__WEBPACK_IMPORTED_MODULE_1__.pt2px)(o):s);if(m!==s){const e=0!==t.width&&0!==t.height?t.width/t.height:1;e>=1?(t.width=m,t.height=m/e):(t.width=m*e,t.height=m)}return t}
-
-
-/***/ })
-
-}]);
+"use strict";(self.webpackChunkdefra=self.webpackChunkdefra||[]).push([[100],{48988:function(e,t,n){n.d(t,{previewWebStyleSymbol:function(){return u}});var i=n(93040),h=n(13520),a=n(63578),s=n(89814);function u(e,t,n){const u=e.thumbnail?.url;return u?(0,i.A)(u,{responseType:"image"}).then((e=>{const t=function(e,t){const n=!/\\.svg$/i.test(e.src)&&t?.disableUpsampling,i=Math.max(e.width,e.height);let s=null!=t?.maxSize?(0,h.Lz)(t.maxSize):a.CB.maxSize;n&&(s=Math.min(i,s));const u="number"==typeof t?.size?t?.size:null,l=Math.min(s,null!=u?(0,h.Lz)(u):i);if(l!==i){const t=0!==e.width&&0!==e.height?e.width/e.height:1;t>=1?(e.width=l,e.height=l/t):(e.width=l*t,e.height=l)}return e}(e.data,n);return n?.node?(n.node.appendChild(t),n.node):t})):(0,s.yG)(e).then((e=>e?t(e,n):null))}}}]);
 //# sourceMappingURL=previewWebStyleSymbol.js.map
